c1e3917229294b4099008894e75b640bc56c9f07
LEGRAND Hugo <legrand.hugo1732@gmail.com>
2022-12-14 11:34:31 +0100

    Grosse update de l'ihm et du metier
    Ajout de ecrireXML
    
    Co-authored-by: lesguardian <lesguardian@users.noreply.github.com>
    Co-authored-by: Aymeric Leber <AymericLeber@users.noreply.github.com>

--- metier/Noeud.java
+++ metier/Noeud.java
@@ -1,24 +1,33 @@
 package metier;
 
-public class Noeud {
-    String nom;
+public class Noeud 
+{
+	String nom;
+	int x;
+	int y;
 
-    public Noeud(String nom)
-    {
-        this.nom = nom;
-    }
+	public Noeud(String nom, int x, int y)
+	{
+		this.nom = nom;
+		this.x = x;
+		this.y = y;
+	}
 
-    public String getNom() {
-        return this.nom;
-    }
+	public String getNom() 	{return this.nom;}
+	public int 	  getX() 	{return this.x;}
+	public int 	  getY() 	{return this.y;}
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+	public void setX(int x) {this.x = x;}
+	public void setY(int y) {this.y = y;}
 
-    public String toString() {
-        return "{" +
-            " nom='" + getNom() + "'" +
-            "}";
-    }
+	@Override
+	public String toString() 
+	{
+		return "{" +
+			" nom='" + getNom() + "'" +
+			", x='" + getX() + "'" +
+			", y='" + getY() + "'" +
+			"}";
+	}
+
 }
