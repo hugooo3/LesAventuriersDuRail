6125fbf8e24f1483f3915540b911d2d1568aa576
Aymeric <aymeric.leb76@gmail.com>
2022-12-14 21:37:26 +0100

    Update lisibilité du code MAJ Metier comme il était avant le push de Maxime

--- metier/CarteWagon.java
+++ metier/CarteWagon.java
@@ -1,39 +1,22 @@
 package metier;
 
 public class CarteWagon {
-    String couleur;
-    String imgRecto;
-    String imgVerso;
+	String couleur;
+	String imgRecto;
+	String imgVerso;
 
-    public CarteWagon(String couleur, String imgRecto, String imgVerso)
-    {
-        this.couleur = couleur;
-        this.imgRecto = imgRecto;
-        this.imgVerso = imgVerso;
-    }
+	public CarteWagon(String couleur, String imgRecto, String imgVerso)
+	{
+		this.couleur = couleur;
+		this.imgRecto = imgRecto;
+		this.imgVerso = imgVerso;
+	}
 
-    public String getCouleur() {
-        return this.couleur;
-    }
+	public String getCouleur() {return this.couleur;}	 
+	public String getImgRecto() {return this.imgRecto;}
+	public String getImgVerso() {return this.imgVerso;}
 
-    public void setCouleur(String couleur) {
-        this.couleur = couleur;
-    }
-     
-    public String getImgRecto() {
-        return this.imgRecto;
-    }
-
-    public void setImgRecto(String imgRecto) {
-        this.imgRecto = imgRecto;
-    }
-
-    public String getImgVerso() {
-        return this.imgVerso;
-    }
-
-    public void setImgVerso(String imgVerso) {
-        this.imgVerso = imgVerso;
-    }
-
+	public void setCouleur(String couleur) {this.couleur = couleur;}
+	public void setImgRecto(String imgRecto) {this.imgRecto = imgRecto;}
+	public void setImgVerso(String imgVerso) {this.imgVerso = imgVerso;}
 }
--- metier/Mappe.java
+++ metier/Mappe.java
@@ -3,98 +3,46 @@ package metier;
 import java.util.ArrayList;
 
 public class Mappe {
-    ArrayList<Noeud> alNoeuds;
-    ArrayList<Arete> alAretes;
-    ArrayList<CarteDestination> alCartesDestination;
-    ArrayList<CarteWagon>       alCartesWagon;
-    String imgMappe;
-    int nbJoueurMin;
-    int nbJoueurMax;
-    int nbJoueurDoubleVoies;
-    int nbWagonJoueur;
+	ArrayList<Noeud> alNoeuds;
+	ArrayList<Arete> alAretes;
+	ArrayList<CarteDestination> alCartesDestination;
+	ArrayList<CarteWagon>       alCartesWagon;
+	String imgMappe;
+	int nbJoueurMin;
+	int nbJoueurMax;
+	int nbJoueurDoubleVoies;
+	int nbWagonJoueur;
 
-    public Mappe(ArrayList<Noeud> alNoeuds, ArrayList<Arete> alAretes, ArrayList<CarteDestination> alCartesDestination, ArrayList<CarteWagon> alCartesWagon, String imgMappe, int nbJoueurMin, int nbJoueurMax, int nbJoueurDoubleVoies, int nbWagonJoueur)
-    {
-        this.alNoeuds = alNoeuds;
-        this.alAretes = alAretes;
-        this.alCartesDestination = alCartesDestination;
-        this.alCartesWagon = alCartesWagon;
-        this.imgMappe = imgMappe;
-        this.nbJoueurMin = nbJoueurMin;
-        this.nbJoueurMax = nbJoueurMax;
-        this.nbJoueurDoubleVoies = nbJoueurDoubleVoies;
-        this.nbWagonJoueur = nbWagonJoueur;
-    }
+	public Mappe(ArrayList<Noeud> alNoeuds, ArrayList<Arete> alAretes, ArrayList<CarteDestination> alCartesDestination, ArrayList<CarteWagon> alCartesWagon, String imgMappe, int nbJoueurMin, int nbJoueurMax, int nbJoueurDoubleVoies, int nbWagonJoueur)
+	{
+		this.alNoeuds = alNoeuds;
+		this.alAretes = alAretes;
+		this.alCartesDestination = alCartesDestination;
+		this.alCartesWagon = alCartesWagon;
+		this.imgMappe = imgMappe;
+		this.nbJoueurMin = nbJoueurMin;
+		this.nbJoueurMax = nbJoueurMax;
+		this.nbJoueurDoubleVoies = nbJoueurDoubleVoies;
+		this.nbWagonJoueur = nbWagonJoueur;
+	}
 
-    public ArrayList<Noeud> getAlNoeuds() {
-        return this.alNoeuds;
-    }
+	public ArrayList<Noeud> getAlNoeuds() {return this.alNoeuds;}
+	public ArrayList<Arete> getAlAretes() {return this.alAretes;}
+	public ArrayList<CarteDestination> getAlCartesDestination() {return this.alCartesDestination;}
+	public ArrayList<CarteWagon> getAlCartesWagon() {return this.alCartesWagon;}
+	public String getImgMappe() {return this.imgMappe;}
+	public int getNbJoueurMin() {return this.nbJoueurMin;}
+	public int getNbJoueurMax() {return this.nbJoueurMax;}
+	public int getNbJoueurDoubleVoies() {return this.nbJoueurDoubleVoies;}
+	public int getNbWagonJoueur() {return this.nbWagonJoueur;}
 
-    public void setAlNoeuds(ArrayList<Noeud> alNoeuds) {
-        this.alNoeuds = alNoeuds;
-    }
-
-    public ArrayList<Arete> getAlAretes() {
-        return this.alAretes;
-    }
-
-    public void setAlAretes(ArrayList<Arete> alAretes) {
-        this.alAretes = alAretes;
-    }
-
-    public ArrayList<CarteDestination> getAlCartesDestination() {
-        return this.alCartesDestination;
-    }
-
-    public void setAlCartesDestination(ArrayList<CarteDestination> alCartesDestination) {
-        this.alCartesDestination = alCartesDestination;
-    }
-
-    public ArrayList<CarteWagon> getAlCartesWagon() {
-        return this.alCartesWagon;
-    }
-
-    public void setAlCartesWagon(ArrayList<CarteWagon> alCartesWagon) {
-        this.alCartesWagon = alCartesWagon;
-    }
-
-    public String getImgMappe() {
-        return this.imgMappe;
-    }
-
-    public void setImgMappe(String imgMappe) {
-        this.imgMappe = imgMappe;
-    }
-
-    public int getNbJoueurMin() {
-        return this.nbJoueurMin;
-    }
-
-    public void setNbJoueurMin(int nbJoueurMin) {
-        this.nbJoueurMin = nbJoueurMin;
-    }
-
-    public int getNbJoueurMax() {
-        return this.nbJoueurMax;
-    }
-
-    public void setNbJoueurMax(int nbJoueurMax) {
-        this.nbJoueurMax = nbJoueurMax;
-    }
-
-    public int getNbJoueurDoubleVoies() {
-        return this.nbJoueurDoubleVoies;
-    }
-
-    public void setNbJoueurDoubleVoies(int nbJoueurDoubleVoies) {
-        this.nbJoueurDoubleVoies = nbJoueurDoubleVoies;
-    }
-
-    public int getNbWagonJoueur() {
-        return this.nbWagonJoueur;
-    }
-
-    public void setNbWagonJoueur(int nbWagonJoueur) {
-        this.nbWagonJoueur = nbWagonJoueur;
-    }
+	public void setAlNoeuds(ArrayList<Noeud> alNoeuds) {this.alNoeuds = alNoeuds;}
+	public void setAlAretes(ArrayList<Arete> alAretes) {this.alAretes = alAretes;}
+	public void setAlCartesDestination(ArrayList<CarteDestination> alCartesDestination) {this.alCartesDestination = alCartesDestination;}
+	public void setAlCartesWagon(ArrayList<CarteWagon> alCartesWagon) {this.alCartesWagon = alCartesWagon;}
+	public void setImgMappe(String imgMappe) {this.imgMappe = imgMappe;}
+	public void setNbJoueurMin(int nbJoueurMin) {this.nbJoueurMin = nbJoueurMin;}
+	public void setNbJoueurMax(int nbJoueurMax) {this.nbJoueurMax = nbJoueurMax;}
+	public void setNbJoueurDoubleVoies(int nbJoueurDoubleVoies) {this.nbJoueurDoubleVoies = nbJoueurDoubleVoies;}
+	public void setNbWagonJoueur(int nbWagonJoueur) {this.nbWagonJoueur = nbWagonJoueur;}
 }
