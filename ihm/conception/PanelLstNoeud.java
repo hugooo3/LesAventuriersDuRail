04a574a19ead008532728c3fdcd9cb85e8dc5730
Beaufils Valentin <valentin.beaufils76600@gmail.com>
2022-12-15 15:10:19 +0100

    Affichage du panel avec liste de Noeud

--- ihm/conception/PanelLstNoeud.java
+++ ihm/conception/PanelLstNoeud.java
@@ -18,7 +18,7 @@ public class PanelLstNoeud extends JPanel {
 	public PanelLstNoeud(int largeur, int hauteur) {
 
 		this.setBackground(Color.LIGHT_GRAY);
-		this.setPreferredSize(new Dimension((int) (largeur * 0.3), hauteur - 20));
+		this.setPreferredSize(new Dimension((int) (largeur * 0.3), hauteur));
 		this.setLayout(null);
 
 		ArrayList<Noeud> test = new ArrayList<Noeud>();
@@ -27,26 +27,52 @@ public class PanelLstNoeud extends JPanel {
 		test.add(new Noeud("Test2", 1, 1));
 		test.add(new Noeud("Test3", 1, 1));
 		test.add(new Noeud("Test", 1, 1));
-		this.setLayout(new BorderLayout());
+		test.add(new Noeud("Test", 1, 1));
+		test.add(new Noeud("Test1", 1, 1));
+		test.add(new Noeud("Test2", 1, 1));
+		test.add(new Noeud("Test3", 1, 1));
+		test.add(new Noeud("Test", 1, 1));
 		this.lstNoeud = new JList(test.toArray());
 
+		// Bouton suivant
 		this.btnSuivant = new JButton("Suivant");
 		// this.btnSuivant.addActionListener(this);
 		this.btnSuivant.setBackground(Color.GRAY);
 		this.btnSuivant.setBorderPainted(false);
 		this.btnSuivant.setFocusPainted(false);
 
+		// JList avec scroll
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setViewportView(lstNoeud);
 		lstNoeud.setLayoutOrientation(JList.VERTICAL);
 		lstNoeud.setVisibleRowCount(3);
-		lstNoeud.setBounds((int) (largeur * 0.3) / 2 - 100, 40, 200, 20);
+		lstNoeud.setFont(new Font(getFont().getName(), Font.PLAIN, 20));
+		lstNoeud.setFixedCellHeight(50);
+
+		// Bouton supprimer
+		JButton btnSuppr = new JButton("Supprimer");
+		btnSuppr.setBackground(Color.GRAY);
+		// btnSuppr.addActionListener(this);
+		btnSuppr.setBorderPainted(false);
+		btnSuppr.setFocusPainted(false);
+
+		// Bouton Modifier
+		JButton btnModif = new JButton("Modifier");
+		btnModif.setBackground(Color.GRAY);
+		// btnModif.addActionListener(this);
+		btnModif.setBorderPainted(false);
+		btnModif.setFocusPainted(false);
+
 		this.add(scrollPane);
+		scrollPane.setBounds((int) (largeur * 0.3) / 2 - 150, 50, 300, 400);
+		this.add(btnSuppr);
+		btnSuppr.setBounds((int) (largeur * 0.3) / 2 - 75, hauteur - 220, 150, 50);
+		this.add(btnModif);
+		btnModif.setBounds((int) (largeur * 0.3) / 2 - 175, hauteur - 120, 150, 50);
 		this.add(this.btnSuivant);
-		this.btnSuivant.setBounds((int) (largeur * 0.3) / 2 - 100, 300, 200, 20);
+		this.btnSuivant.setBounds((int) (largeur * 0.3) / 2 + 25, hauteur - 120, 150, 50);
 
 		// scrollPane.setSize(new Dimension(200, 300));
-		// scrollPane.setBounds((int) (largeur * 0.3) / 2 - 100, 40, 200, 20);
 
 		// this.lstNoeud.setPreferredSize(new Dimension((int) (largeur * 0.3),
 		//
