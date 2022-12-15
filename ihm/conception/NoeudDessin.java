5f1eafdfbe534a885bd611a00275a141605d3b74
Aymeric <aymeric.leb76@gmail.com>
2022-12-15 15:34:53 +0100

    Déplacement à la souris des noeuds

--- ihm/conception/NoeudDessin.java
+++ ihm/conception/NoeudDessin.java
@@ -6,18 +6,44 @@ import metier.Noeud;
 public class NoeudDessin extends Noeud
 {
 	private int radius;
+	private int ellipseX;
+	private int ellipseY;
 	private Ellipse2D ellipse2D;
 
 	public NoeudDessin(String nom, int x, int y, int radius) 
 	{
 		super(nom, x, y);
+		this.ellipseX = x;
+		this.ellipseY = y;
 
 		this.radius = radius;
-		this.ellipse2D = new Ellipse2D.Double(x + (radius / 2), y + (radius / 2), radius, radius);
+		this.ellipse2D = new Ellipse2D.Double(this.ellipseX + (radius / 2), this.ellipseY + (radius / 2), radius, radius);
 	}
 
 	public int getRadius() {return radius;}
 	public Ellipse2D getEllipse2D() {return ellipse2D;}
+	public int getEllipseX() {return this.ellipseX;}
+	public int getEllipseY() {return this.ellipseY;}
+
+	public void setX(int x) {super.x = x - this.radius;}
+	public void setY(int y) {super.y = y - this.radius;}
+
+	public void setEllipseX(int ellipseX) 
+	{
+		this.ellipseX = ellipseX - this.radius;
+		this.majEllipse2D();
+	}
+
+	public void setEllipseY(int ellipseY) 
+	{
+		this.ellipseY = ellipseY - this.radius;
+		this.majEllipse2D();
+	}
+
+	public void majEllipse2D()
+	{
+		this.ellipse2D = new Ellipse2D.Double(this.ellipseX + (radius / 2), this.ellipseY + (radius / 2), radius, radius);
+	}
 
 	@Override
 	public String toString() {
--- ihm/conception/PanelMappe.java
+++ ihm/conception/PanelMappe.java
@@ -4,24 +4,25 @@ import metier.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.*;
 import java.awt.event.*;
-import java.awt.image.*;
 import java.io.File;
 import java.util.ArrayList;
 
-public class PanelMappe extends JPanel {
+public class PanelMappe extends JPanel 
+{
 	private Image img;
 	private int largeur;
 	private int hauteur;
 	private boolean cliquable;
+	private NoeudDessin noeudSelec = null;
 
 	private Frame frame;
 
 	public ArrayList<NoeudDessin> alNoeudDessin = new ArrayList<NoeudDessin>();
 	public ArrayList<Noeud> alNoeud = new ArrayList<Noeud>();
 
-	public PanelMappe(Frame frame, File imagePath, int largeur, int hauteur, boolean cliquable) {
+	public PanelMappe(Frame frame, File imagePath, int largeur, int hauteur, boolean cliquable) 
+	{
 		this.frame = frame;
 		this.largeur = largeur;
 		this.hauteur = hauteur;
@@ -29,14 +30,13 @@ public class PanelMappe extends JPanel {
 
 		this.setLayout(null);
 		this.img = getToolkit().getImage(imagePath.getAbsolutePath());
-		this.setPreferredSize(new Dimension((int) (largeur * 0.7), hauteur));
+		this.setPreferredSize(new Dimension((int) (this.largeur * 0.7), this.hauteur));
 
-		if (cliquable) {
+		if (this.cliquable) 
+		{
 			// Dessin sur le Panel
-			this.addMouseListener(new MouseAdapter() {
-
-				private NoeudDessin noeudSelec = null;
-
+			this.addMouseListener(new MouseAdapter() 
+			{
 				@Override
 				public void mousePressed(MouseEvent e) 
 				{
@@ -44,42 +44,61 @@ public class PanelMappe extends JPanel {
 					{
 						if (noeudDessin.getEllipse2D().contains(e.getPoint()))
 						{
-							JOptionPane.showMessageDialog(frame, "Un noeud existe déjà à cet emplacement");
+							//JOptionPane.showMessageDialog(frame, "Un noeud existe déjà à cet emplacement");
+							PanelMappe.this.noeudSelec = noeudDessin;
 							return;
 						}
 					}
 
 					JLabel lblNom = new JLabel("Nom du Noeud : ");
-					String getMessage = JOptionPane.showInputDialog(frame, lblNom, "Création d'un Noeud", JOptionPane.QUESTION_MESSAGE);
+					String getMessage = JOptionPane.showInputDialog(PanelMappe.this.frame, lblNom, "Création d'un Noeud", JOptionPane.QUESTION_MESSAGE);
 					if (getMessage != null)
 					{
-						this.noeudSelec = new NoeudDessin(getMessage, e.getX() - 20, e.getY() - 20, 20);
-						PanelMappe.this.alNoeudDessin.add(this.noeudSelec);
+						PanelMappe.this.noeudSelec = new NoeudDessin(getMessage, e.getX() - 20, e.getY() - 20, 20);
+						PanelMappe.this.alNoeudDessin.add(PanelMappe.this.noeudSelec);
 						repaint();
 					}
 				}
 
 				@Override
-				public void mouseReleased(MouseEvent e) {
-					this.noeudSelec = null;
+				public void mouseReleased(MouseEvent e) {PanelMappe.this.noeudSelec = null;}
+			});
+
+			this.addMouseMotionListener(new MouseMotionAdapter() 
+			{			
+				public void mouseDragged(MouseEvent e) 
+				{
+	 				if (PanelMappe.this.noeudSelec != null)
+					{
+						PanelMappe.this.noeudSelec.setX(e.getX());
+						PanelMappe.this.noeudSelec.setY(e.getY());
+
+						PanelMappe.this.noeudSelec.setEllipseX(e.getX());
+						PanelMappe.this.noeudSelec.setEllipseY(e.getY());
+			
+						repaint();
+					}
 				}
 			});
 		}
 	}
 
-	public void paint(Graphics g) {
+	public void paint(Graphics g) 
+	{
 		super.paintComponent(g);
 
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.drawImage(img, 1, 1, (int) this.getSize().getWidth() - 2, (int) this.getSize().getHeight() - 2, this);
 
 		System.out.println(this.alNoeudDessin.size());
-		for (NoeudDessin noeud : this.alNoeudDessin) {
-
+		for (NoeudDessin noeud : this.alNoeudDessin) 
+		{
 			g2d.setColor(Color.RED);
 			g2d.fill(noeud.getEllipse2D());
 			g2d.setColor(Color.YELLOW);
 			g2d.draw(noeud.getEllipse2D());
+			g2d.setColor(Color.BLACK);
+			g2d.drawString(noeud.getNom(), noeud.getX(), noeud.getY());
 		}
 	}
 }
