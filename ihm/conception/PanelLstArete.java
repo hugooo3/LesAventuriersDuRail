edc10dd1abbb547d88cbe9ec4010650bb14540ba
Beaufils Valentin <valentin.beaufils76600@gmail.com>
2023-01-03 12:16:38 +0100

    Commit du midi Alors : - Enlever les aretes après enlever le noeud - Voie double en cours.. - Avancement CarteDesti
    Co-authored-by: Maxime Brochard <MaximeBro@users.noreply.github.com>
    Co-authored-by: lesguardian <lesguardian@users.noreply.github.com>
    Co-authored-by: Aymeric Leber <AymericLeber@users.noreply.github.com>

--- ihm/conception/PanelLstArete.java
+++ ihm/conception/PanelLstArete.java
@@ -9,8 +9,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class PanelLstArete extends JPanel implements ActionListener 
-{
+public class PanelLstArete extends JPanel implements ActionListener {
 	private FrameConcepteur concepteur;
 
 	private JButton btnNouveau, btnModif, btnSuppr;
@@ -24,13 +23,13 @@ public class PanelLstArete extends JPanel implements ActionListener
 	private JComboBox<Noeud> ddlstNoeud1;
 	private JComboBox<Noeud> ddlstNoeud2;
 	private JComboBox<CarteWagon> ddlstCouleur;
+	private JComboBox<CarteWagon> ddlstCouleurVoieDouble;
 	private JTextField txtNbWagon;
 	private JCheckBox cbVoieDouble;
-	
+
 	private GridBagConstraints gbc = new GridBagConstraints();
 
-	public PanelLstArete(FrameConcepteur concepteur, int largeur, int hauteur) 
-	{
+	public PanelLstArete(FrameConcepteur concepteur, int largeur, int hauteur) {
 		this.concepteur = concepteur;
 
 		this.setBackground(Color.LIGHT_GRAY);
@@ -45,7 +44,6 @@ public class PanelLstArete extends JPanel implements ActionListener
 		this.lstArete = new JList<Arete>(this.modelListArete);
 		this.lstArete.setCellRenderer(new RendererCouleur());
 
-
 		this.panelPopUp = new JPanel(new GridBagLayout());
 		GridBagConstraints gbcPopUp = new GridBagConstraints();
 		gbcPopUp.insets = new Insets(5, 2, 5, 2);
@@ -57,13 +55,21 @@ public class PanelLstArete extends JPanel implements ActionListener
 		this.ddlstNoeud2 = new JComboBox<Noeud>(this.alNoeud.toArray(new Noeud[this.alNoeud.size()]));
 		this.ddlstNoeud1.setPrototypeDisplayValue(new Noeud("XXXXXXXXXXXXXXXXXXXX", 0, 0));
 		this.ddlstNoeud2.setPrototypeDisplayValue(new Noeud("XXXXXXXXXXXXXXXXXXXX", 0, 0));
-		
+
 		this.ddlstCouleur = new JComboBox<CarteWagon>(alCouleur.toArray(new CarteWagon[alCouleur.size()]));
 		this.ddlstCouleur.removeItemAt(alCouleur.size() - 1); // Joker retire des choix possibles
 		this.ddlstCouleur.setRenderer(new RendererCouleur());
 
+		this.ddlstCouleurVoieDouble = new JComboBox<CarteWagon>(alCouleur.toArray(new CarteWagon[alCouleur.size()]));
+		this.ddlstCouleurVoieDouble.removeItemAt(alCouleur.size() - 1); // Joker retire des choix possibles
+		this.ddlstCouleurVoieDouble.setRenderer(new RendererCouleur());
+		this.ddlstCouleurVoieDouble.setEnabled(false);
+
 		this.txtNbWagon = new JTextField("1", 5);
+		JLabel lblNbWagon = new JLabel("Nb Wagon :");
+		lblNbWagon.setLabelFor(this.txtNbWagon);
 		this.cbVoieDouble = new JCheckBox("Voie double");
+		this.cbVoieDouble.addActionListener(this);
 
 		// ComboBox Noeud 1
 		gbcPopUp.gridx = 0;
@@ -80,17 +86,25 @@ public class PanelLstArete extends JPanel implements ActionListener
 		gbcPopUp.gridy = 1;
 		this.panelPopUp.add(this.ddlstCouleur, gbcPopUp);
 
-		// TextField NbWagon
-		gbcPopUp.gridx = 1;
-		gbcPopUp.gridy = 1;
-		this.panelPopUp.add(this.txtNbWagon, gbcPopUp);
-
 		// CheckBox Voie Double
-		gbcPopUp.gridx = 2;
+		gbcPopUp.gridx = 1;
 		gbcPopUp.gridy = 1;
 		this.panelPopUp.add(this.cbVoieDouble, gbcPopUp);
 
+		// ComboBox Couleur Voie Double
+		gbcPopUp.gridx = 2;
+		gbcPopUp.gridy = 1;
+		this.panelPopUp.add(this.ddlstCouleurVoieDouble, gbcPopUp);
 
+		// Label txtNbWagon
+		gbcPopUp.gridx = 0;
+		gbcPopUp.gridy = 2;
+		this.panelPopUp.add(lblNbWagon, gbcPopUp);
+
+		// TextField NbWagon
+		gbcPopUp.gridx = 1;
+		gbcPopUp.gridy = 2;
+		this.panelPopUp.add(this.txtNbWagon, gbcPopUp);
 
 		// JList avec scroll
 		this.scrollPane = new JScrollPane();
@@ -129,7 +143,7 @@ public class PanelLstArete extends JPanel implements ActionListener
 		this.gbc.gridy = 1;
 		this.gbc.gridwidth = 1;
 		this.add(this.btnNouveau, this.gbc);
-		
+
 		this.gbc.gridx = 1;
 		this.gbc.gridy = 1;
 		this.add(this.btnModif, this.gbc);
@@ -139,96 +153,120 @@ public class PanelLstArete extends JPanel implements ActionListener
 		this.add(this.btnSuppr, this.gbc);
 	}
 
-	public void majLstArete() 
-	{
-		for (Arete arete : this.alAretes)
-		{
-			if (!this.modelListArete.contains(arete))
-			{
+	public void majLstArete() {
+		for (Arete arete : this.alAretes) {
+			if (!this.modelListArete.contains(arete)) {
 				this.modelListArete.addElement(arete);
 			}
 		}
 		this.lstArete.clearSelection();
 	}
 
-	private void majComboBox()
-	{
+	private void majComboBox() {
 		this.ddlstNoeud1.removeAllItems();
 		this.ddlstNoeud2.removeAllItems();
-		for (Noeud noeud : this.alNoeud)
-		{
+		for (Noeud noeud : this.alNoeud) {
 			this.ddlstNoeud1.addItem(noeud);
 			this.ddlstNoeud2.addItem(noeud);
 		}
 	}
 
+	// Enleve les arretes correspondantes au noeud
+	public void removeLstNoeud(Noeud noeud) {
+		for (int i = 0; i < this.alAretes.size(); i++) {
+			if (this.alAretes.get(i).getNoeud1().equals(noeud) ||
+					this.alAretes.get(i).getNoeud2().equals(noeud)) {
+				this.alAretes.remove(i);
+				i--;
+			}
+
+			if (this.modelListArete.get(i).getNoeud1().equals(noeud)
+					|| this.modelListArete.get(i).getNoeud2().equals(noeud)) {
+				this.modelListArete.remove(i);
+				i--;
+			}
+			this.concepteur.majIHM();
+		}
+	}
+
 	@Override
-	public void actionPerformed(ActionEvent e) 
-	{
-		if (e.getSource() == this.btnNouveau)
-		{
-			if (this.alNoeud.size() < 2)
-			{
-				JOptionPane.showMessageDialog(null, "Nombre de noeud insuffisant pour faire une arête", "Erreur", JOptionPane.ERROR_MESSAGE);
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.cbVoieDouble) {
+			if (this.cbVoieDouble.isSelected()) {
+				this.ddlstCouleurVoieDouble.setEnabled(true);
+			} else {
+				this.ddlstCouleurVoieDouble.setEnabled(false);
+			}
+		}
+
+		if (e.getSource() == this.btnNouveau) {
+			if (this.alNoeud.size() < 2) {
+				JOptionPane.showMessageDialog(null, "Nombre de noeud insuffisant pour faire une arête", "Erreur",
+						JOptionPane.ERROR_MESSAGE);
 				return;
 			}
 
 			this.majComboBox();
 
-			do 
-			{
+			do {
 				this.ddlstNoeud1.setSelectedIndex(0);
 				this.ddlstNoeud2.setSelectedIndex(1);
-				int n = JOptionPane.showOptionDialog(this, this.panelPopUp, "Création d'une arête" ,JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
-				
-				if ((Noeud)this.ddlstNoeud1.getSelectedItem() == (Noeud)this.ddlstNoeud2.getSelectedItem())
-					JOptionPane.showMessageDialog(null, "Les deux noeuds sélectionnés sont identiques", "Erreur", JOptionPane.ERROR_MESSAGE);
-				
+				int n = JOptionPane.showOptionDialog(this, this.panelPopUp, "Création d'une arête",
+						JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+				if ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem())
+					JOptionPane.showMessageDialog(null, "Les deux noeuds sélectionnés sont identiques", "Erreur",
+							JOptionPane.ERROR_MESSAGE);
+
 				if (n != JOptionPane.OK_OPTION) // Cancel ou croix == annulation
 					return;
-			}
-			while ((Noeud)this.ddlstNoeud1.getSelectedItem() == (Noeud)this.ddlstNoeud2.getSelectedItem());
+			} while ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem());
 
-
-			Noeud noeudSelected1 = (Noeud)this.ddlstNoeud1.getSelectedItem();
-			Noeud noeudSelected2 = (Noeud)this.ddlstNoeud2.getSelectedItem();
-			CarteWagon carteWagon = (CarteWagon)this.ddlstCouleur.getSelectedItem();
+			Noeud noeudSelected1 = (Noeud) this.ddlstNoeud1.getSelectedItem();
+			Noeud noeudSelected2 = (Noeud) this.ddlstNoeud2.getSelectedItem();
+			CarteWagon couleur = (CarteWagon) this.ddlstCouleur.getSelectedItem();
+			CarteWagon couleurDoubleVoie = (CarteWagon) this.ddlstCouleurVoieDouble.getSelectedItem();
 			int nbWagon = Integer.parseInt(this.txtNbWagon.getText());
 
-			this.alAretes.add(new Arete(noeudSelected1, noeudSelected2, carteWagon, nbWagon, this.cbVoieDouble.isSelected()));
+			if (this.cbVoieDouble.isSelected()) {
+				this.alAretes.add(
+						new Arete(noeudSelected1, noeudSelected2, couleur, nbWagon,
+								this.cbVoieDouble.isSelected(), couleurDoubleVoie));
+			} else {
+				this.alAretes.add(new Arete(noeudSelected1, noeudSelected2, couleur, nbWagon,
+						this.cbVoieDouble.isSelected(), null));
+			}
 			this.concepteur.majIHM();
 		}
 
-		if (e.getSource() == this.btnModif)
-		{
-			if (this.lstArete.getSelectedIndex() != -1)
-			{
+		if (e.getSource() == this.btnModif) {
+			if (this.lstArete.getSelectedIndex() != -1) {
 				this.majComboBox();
 
 				Arete areteSelected = this.lstArete.getSelectedValue();
 
 				this.ddlstCouleur.setSelectedItem(areteSelected.getCouleur());
 				this.txtNbWagon.setText(Integer.toString(areteSelected.getTroncons()));
-				this.cbVoieDouble.setSelected(areteSelected.getVoieDouble());	
+				this.cbVoieDouble.setSelected(areteSelected.getVoieDouble());
 
-				do
-				{
+				do {
 					this.ddlstNoeud1.setSelectedItem(areteSelected.getNoeud1());
 					this.ddlstNoeud2.setSelectedItem(areteSelected.getNoeud2());
 
-					int n = JOptionPane.showOptionDialog(this, this.panelPopUp, "Modification d'une arête" , JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+					int n = JOptionPane.showOptionDialog(this, this.panelPopUp, "Modification d'une arête",
+							JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
 
-					if ((Noeud)this.ddlstNoeud1.getSelectedItem() == (Noeud)this.ddlstNoeud2.getSelectedItem())
-						JOptionPane.showMessageDialog(null, "Les deux noeuds sélectionnés sont identiques", "Erreur", JOptionPane.ERROR_MESSAGE);
+					if ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem())
+						JOptionPane.showMessageDialog(null, "Les deux noeuds sélectionnés sont identiques", "Erreur",
+								JOptionPane.ERROR_MESSAGE);
 
 					if (n != JOptionPane.OK_OPTION) // Cancel ou croix == annulation
 						return;
-				}
-				while ((Noeud)this.ddlstNoeud1.getSelectedItem() == (Noeud)this.ddlstNoeud2.getSelectedItem());
+				} while ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem());
 
-				areteSelected.setNoeud1((Noeud)this.ddlstNoeud1.getSelectedItem());
-				areteSelected.setNoeud2((Noeud)this.ddlstNoeud2.getSelectedItem());
-				areteSelected.setCouleur((CarteWagon)this.ddlstCouleur.getSelectedItem());
+				areteSelected.setNoeud1((Noeud) this.ddlstNoeud1.getSelectedItem());
+				areteSelected.setNoeud2((Noeud) this.ddlstNoeud2.getSelectedItem());
+				areteSelected.setCouleur((CarteWagon) this.ddlstCouleur.getSelectedItem());
 				areteSelected.setTroncons(Integer.parseInt(this.txtNbWagon.getText()));
 				areteSelected.setVoieDouble(this.cbVoieDouble.isSelected());
 
@@ -236,28 +274,29 @@ public class PanelLstArete extends JPanel implements ActionListener
 			}
 		}
 
-		if (e.getSource() == this.btnSuppr)
-		{	
-			if (this.lstArete.getSelectedIndex() != -1)
-			{
+		if (e.getSource() == this.btnSuppr) {
+			if (this.lstArete.getSelectedIndex() != -1) {
 				Arete areteSelect = this.lstArete.getSelectedValue();
 				this.alAretes.remove(areteSelect);
 				this.modelListArete.removeElement(areteSelect);
 				this.concepteur.majIHM();
 			}
 		}
-/* 		// Clic sur le bouton Suivant
-		if (e.getSource() == this.btnSuivant) 
-		{
-			System.out.println("Clic sur le bouton suivant");
-			ArrayList<Noeud> alAretes = new ArrayList<Noeud>(lstArete.getModel().getSize());
-
-			for (int i = 0; i < lstArete.getModel().getSize(); i++) {
-				alstArete.add(lstArete.getModel().getElementAt(i));
-			}
-
-			// this.frame.creeralAretes(alAretes);
-			// this.frame.verifMAJ("noeud");
-		} */
+		/*
+		 * // Clic sur le bouton Suivant
+		 * if (e.getSource() == this.btnSuivant)
+		 * {
+		 * System.out.println("Clic sur le bouton suivant");
+		 * ArrayList<Noeud> alAretes = new
+		 * ArrayList<Noeud>(lstArete.getModel().getSize());
+		 * 
+		 * for (int i = 0; i < lstArete.getModel().getSize(); i++) {
+		 * alstArete.add(lstArete.getModel().getElementAt(i));
+		 * }
+		 * 
+		 * // this.frame.creeralAretes(alAretes);
+		 * // this.frame.verifMAJ("noeud");
+		 * }
+		 */
 	}
 }
--- ihm/conception/PanelLstCarteDestination.java
+++ ihm/conception/PanelLstCarteDestination.java
@@ -3,6 +3,8 @@ package ihm.conception;
 import metier.*;
 import ihm.FrameConcepteur;
 
+import java.io.File;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -10,22 +12,27 @@ import javax.swing.*;
 
 import java.util.ArrayList;
 
-public class PanelLstCarteDestination extends JPanel implements ActionListener
-{
+public class PanelLstCarteDestination extends JPanel implements ActionListener {
 	private FrameConcepteur concepteur;
 
 	private JList<CarteDestination> lstCarteDestination;
 	private ArrayList<CarteDestination> alCarteDestination;
 
+	private JPanel panelPopUpDesti;
 	private JScrollPane scrollPane;
 	private DefaultListModel<CarteDestination> modelListCarteDestination;
 
+	private JComboBox<Noeud> ddlstNoeud1;
+	private JComboBox<Noeud> ddlstNoeud2;
+	private ArrayList<Noeud> alNoeud;
+
 	private JButton btnNouveau, btnModif, btnSuppr, btnAuto;
-	
+
+	private JTextField txtNbPoint;
+
 	private GridBagConstraints gbc = new GridBagConstraints();
 
-	public PanelLstCarteDestination(FrameConcepteur concepteur, int largeur, int hauteur) 
-	{
+	public PanelLstCarteDestination(FrameConcepteur concepteur, int largeur, int hauteur) {
 		this.concepteur = concepteur;
 
 		this.setBackground(Color.LIGHT_GRAY);
@@ -38,6 +45,42 @@ public class PanelLstCarteDestination extends JPanel implements ActionListener
 
 		this.modelListCarteDestination = new DefaultListModel<CarteDestination>();
 		this.lstCarteDestination = new JList<CarteDestination>(this.modelListCarteDestination);
+
+		// Création de la PopUp
+
+		this.panelPopUpDesti = new JPanel(new GridBagLayout());
+		GridBagConstraints gbcPopUp = new GridBagConstraints();
+		gbcPopUp.insets = new Insets(5, 2, 5, 2);
+
+		this.txtNbPoint = new JTextField("5", 5);
+		JLabel lblNbPoint = new JLabel("Nombre de points :");
+		lblNbPoint.setLabelFor(this.txtNbPoint);
+
+		this.alNoeud = this.concepteur.getMetier().getAlNoeuds();
+		this.ddlstNoeud1 = new JComboBox<Noeud>(this.alNoeud.toArray(new Noeud[this.alNoeud.size()]));
+		this.ddlstNoeud2 = new JComboBox<Noeud>(this.alNoeud.toArray(new Noeud[this.alNoeud.size()]));
+		this.ddlstNoeud1.setPrototypeDisplayValue(new Noeud("XXXXXXXXXXXXXXXXXXXX", 0, 0));
+		this.ddlstNoeud2.setPrototypeDisplayValue(new Noeud("XXXXXXXXXXXXXXXXXXXX", 0, 0));
+
+		// Combobox Noeud 1
+		gbcPopUp.gridx = 0;
+		gbcPopUp.gridy = 0;
+		this.panelPopUpDesti.add(this.ddlstNoeud1, gbcPopUp);
+
+		// ComboBox Noeud2
+		gbcPopUp.gridx = 1;
+		gbcPopUp.gridy = 0;
+		this.panelPopUpDesti.add(this.ddlstNoeud2, gbcPopUp);
+
+		// TextField NbPoint
+		gbcPopUp.gridx = 1;
+		gbcPopUp.gridy = 1;
+		this.panelPopUpDesti.add(this.txtNbPoint, gbcPopUp);
+
+		// Label NbPoint
+		gbcPopUp.gridx = 0;
+		gbcPopUp.gridy = 1;
+		this.panelPopUpDesti.add(lblNbPoint, gbcPopUp);
 
 		// JList avec scroll
 		this.scrollPane = new JScrollPane();
@@ -84,7 +127,7 @@ public class PanelLstCarteDestination extends JPanel implements ActionListener
 		this.gbc.gridy = 1;
 		this.gbc.gridwidth = 1;
 		this.add(this.btnNouveau, this.gbc);
-		
+
 		this.gbc.gridx = 1;
 		this.gbc.gridy = 1;
 		this.add(this.btnModif, this.gbc);
@@ -99,35 +142,123 @@ public class PanelLstCarteDestination extends JPanel implements ActionListener
 		this.add(this.btnAuto, this.gbc);
 	}
 
-	public void majLstCarteDestination() 
-	{
-		for (CarteDestination carteDestination : this.alCarteDestination)
-		{
-			if (!this.modelListCarteDestination.contains(carteDestination))
-			{
+	public void majLstCarteDestination() {
+		for (CarteDestination carteDestination : this.alCarteDestination) {
+			if (!this.modelListCarteDestination.contains(carteDestination)) {
 				this.modelListCarteDestination.addElement(carteDestination);
 			}
 		}
 		this.lstCarteDestination.clearSelection();
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) 
-	{
-		if (e.getSource() == this.btnNouveau)
-		{
-			// @TODO
+	private void majComboBox() {
+		this.ddlstNoeud1.removeAllItems();
+		this.ddlstNoeud2.removeAllItems();
+		for (Noeud noeud : this.alNoeud) {
+			this.ddlstNoeud1.addItem(noeud);
+			this.ddlstNoeud2.addItem(noeud);
 		}
-		
-		if (e.getSource() == this.btnModif)
-		{
-			// @TODO
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		this.majComboBox();
+
+		if (e.getSource() == this.btnNouveau) {
+			if (this.ddlstNoeud1.getItemCount() > 0 && this.ddlstNoeud1.getItemCount() > 0)
+				do {
+					this.ddlstNoeud1.setSelectedIndex(0);
+					this.ddlstNoeud2.setSelectedIndex(1);
+					int n = JOptionPane.showOptionDialog(this, this.panelPopUpDesti, "Création d'une carte destination",
+							JOptionPane.OK_CANCEL_OPTION,
+							JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+					if ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem())
+						JOptionPane.showMessageDialog(null, "Les deux noeuds sélectionnés sont identiques", "Erreur",
+								JOptionPane.ERROR_MESSAGE);
+
+					if (n != JOptionPane.OK_OPTION) // Cancel ou croix == annulation
+						return;
+				} while ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem());
+
+			Noeud noeudSelected1 = (Noeud) this.ddlstNoeud1.getSelectedItem();
+			Noeud noeudSelected2 = (Noeud) this.ddlstNoeud2.getSelectedItem();
+			int nbPoints = Integer.parseInt(this.txtNbPoint.getText());
+			File imgRecto = new File("/images/carteDestRecto.png");
+			File imgVerso = new File("/images/carteDestVerso.png");
+
+			CarteDestination carteDestination = new CarteDestination(noeudSelected1, noeudSelected2, nbPoints, imgRecto,
+					imgVerso);
+
+			for (CarteDestination carte : this.alCarteDestination)
+				if (carteDestination.getNoeud1() == carte.getNoeud1() && 
+					carteDestination.getNoeud2() == carte.getNoeud2() &&
+					carteDestination.getPoints() == carte.getPoints()) {
+					
+					JOptionPane.showMessageDialog(null, "Cette carte destination existe déjà", "Erreur",
+
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+
+			this.alCarteDestination.add(carteDestination);
+			this.concepteur.majIHM();
 		}
 
-		if (e.getSource() == this.btnSuppr)
-		{	
-			if (this.lstCarteDestination.getSelectedIndex() != -1)
-			{
+
+
+		if (e.getSource() == this.btnModif) {
+			if (this.lstCarteDestination.getSelectedIndex() != -1) {
+				this.majComboBox();
+
+				CarteDestination carteSelected = (CarteDestination) this.lstCarteDestination.getSelectedValue();
+				this.ddlstNoeud1.setSelectedItem(carteSelected.getNoeud1());
+				this.ddlstNoeud2.setSelectedItem(carteSelected.getNoeud2());
+				this.txtNbPoint.setText(String.valueOf(carteSelected.getPoints()));
+
+				if (this.ddlstNoeud1.getItemCount() > 0 && this.ddlstNoeud1.getItemCount() > 0)
+					do {
+						int n = JOptionPane.showOptionDialog(this, this.panelPopUpDesti,
+								"Modification d'une carte destination",
+								JOptionPane.OK_CANCEL_OPTION,
+								JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+						if ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem())
+							JOptionPane.showMessageDialog(null, "Les deux noeuds sélectionnés sont identiques",
+									"Erreur",
+									JOptionPane.ERROR_MESSAGE);
+
+						if (n != JOptionPane.OK_OPTION) // Cancel ou croix == annulation
+							return;
+					} while ((Noeud) this.ddlstNoeud1.getSelectedItem() == (Noeud) this.ddlstNoeud2.getSelectedItem());
+
+				CarteDestination carteTemp = new CarteDestination((Noeud) this.ddlstNoeud1.getSelectedItem(),
+						(Noeud) this.ddlstNoeud2.getSelectedItem(),
+						Integer.parseInt(this.txtNbPoint.getText()),
+						new File("/images/carteDestRecto.png"),
+						new File("/images/carteDestVerso.png"));
+
+				for (CarteDestination carte : this.alCarteDestination)
+					if (carteTemp.getNoeud1() == carte.getNoeud1() &&
+						carteTemp.getNoeud2() == carte.getNoeud2() &&
+						carteTemp.getPoints() == carte.getPoints()) {
+					
+					JOptionPane.showMessageDialog(null, "Cette carte destination existe déjà", "Erreur", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+
+				carteSelected.setNoeud1((Noeud) this.ddlstNoeud1.getSelectedItem());
+				carteSelected.setNoeud2((Noeud) this.ddlstNoeud2.getSelectedItem());
+				carteSelected.setPoints(Integer.parseInt(this.txtNbPoint.getText()));
+
+				this.concepteur.majIHM();
+
+			}
+		}
+
+		if (e.getSource() == this.btnSuppr) {
+			if (this.lstCarteDestination.getSelectedIndex() != -1) {
 				CarteDestination carteDestinationSelect = this.lstCarteDestination.getSelectedValue();
 				this.alCarteDestination.remove(carteDestinationSelect);
 				this.modelListCarteDestination.removeElement(carteDestinationSelect);
@@ -135,8 +266,7 @@ public class PanelLstCarteDestination extends JPanel implements ActionListener
 			}
 		}
 
-		if (e.getSource() == this.btnAuto)
-		{
+		if (e.getSource() == this.btnAuto) {
 			// @TODO
 		}
 	}
